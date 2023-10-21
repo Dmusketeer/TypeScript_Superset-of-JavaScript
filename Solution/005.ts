5c7d6904afdd3bb08ac9d126ae4d26a07520aa53
Dmuskeeter <tdheeraj9a@gmail.com>
2023-10-21 21:28:47 +0530

    palindrome string

---
+++ Solution/005.ts
@@ -0,0 +1,24 @@
+// 5. Create a TypeScript function to check if a string is a palindrome.
+// str="naman"
+// revStr="naman"
+const palindrome=(str:string):string =>{
+    let newStr=""
+    const cleanedString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase(); // Remove non-alphanumeric characters and convert to lowercase
+    console.log(cleanedString)
+    for(let i=str.length-1; i>=0; i--){
+        newStr+=cleanedString[i]
+    }
+    console.log(newStr)
+    if (cleanedString==newStr) {
+        return "String is palindrome"
+    } else {
+        return "String is Not Palindrome"        
+    }
+}
+
+// Example usage:
+let str1="Racecar"
+console.log(palindrome(str1))
+
+let str2="Dheeraj"
+console.log(palindrome(str2))
