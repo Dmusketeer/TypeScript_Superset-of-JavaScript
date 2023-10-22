87d9e4bfd77f8dfd645d88d6d1cd2fa89970392e
Dmuskeeter <tdheeraj9a@gmail.com>
2023-10-22 19:33:04 +0530

    largest element in an array

---
+++ Solution/007.js
@@ -0,0 +1,12 @@
+// 7. Implement a TypeScript function to find the largest element in an array.
+var largestElement = function (arr) {
+    var largeEle = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] >= largeEle) {
+            largeEle = arr[i];
+        }
+    }
+    return largeEle;
+};
+var arr = [12, 23, 34, 14, 55,];
+console.log(largestElement(arr));
