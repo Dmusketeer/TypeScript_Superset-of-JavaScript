52a97b03149b47c883fa5589889b2db4f6a91c8f
Dmuskeeter <tdheeraj9a@gmail.com>
2023-11-02 13:01:33 +0530

    gcd of two numbers

---
+++ Solution/023.ts
@@ -0,0 +1,19 @@
+// 23. Write a TypeScript program to find the LCM(Least Common Multiple) of two numbers.
+function findGCD(a: number, b: number): number {
+    if (b === 0) {
+        return a;
+    }
+    return findGCD(b, a % b);
+}
+
+function findLCM(a: number, b: number): number {
+    const gcd = findGCD(a, b);
+    return (a * b) / gcd;
+}
+
+// Example usage:
+const num1 = 12; // Replace with your first number
+const num2 = 18; // Replace with your second number
+
+const lcm = findLCM(num1, num2);
+console.log(`The LCM of ${num1} and ${num2} is ${lcm}`);
