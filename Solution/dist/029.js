e0b360d60a4a1087a26f36fdd1fc6c1526636e40
Dmuskeeter <tdheeraj9a@gmail.com>
2023-11-04 11:00:02 +0530

    convert to lower case

---
+++ Solution/dist/029.js
@@ -0,0 +1,6 @@
+// 29. Write a TypeScript function to convert a string to lowercase.
+var convertToLowerCase = function (str) {
+    return str.toLowerCase();
+};
+var str = "Hey! Dheeraj HOW are YOu?";
+console.log(convertToLowerCase(str));
