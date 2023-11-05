b0792da97529b3e06029b56f1f59287f23117d69
Dmuskeeter <tdheeraj9a@gmail.com>
2023-11-05 13:26:47 +0530

    num is prime or not

--- Solution/020.ts
+++ Solution/020.ts
@@ -1,21 +1,28 @@
 // 20. Write a TypeScript function to check if a number is a prime number.
-
-const isPrime = (num: number): boolean => {
-    if (num < 2) {
-        return false
+function isPrime(num: number): boolean {
+    if (num <= 1) {
+        return false; // 0 and 1 are not prime
     }
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % 2 == 0) {
-            return false
+    if (num <= 3) {
+        return true; // 2 and 3 are prime
+    }
+    if (num % 2 === 0 || num % 3 === 0) {
+        return false;
+    }
+
+    // Check for primality using a loop starting from 5 and incrementing by 6
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) {
+            return false;
         }
     }
-    return true
+
+    return true;
 }
 
-
 // Example usage:
-const num1 = 17; // Prime number
-const num2 = 10; // Not a prime number
+const num1 = 6; // Prime number
+const num2 = 9; // Not a prime number
 
 console.log(isPrime(num1)); // true
 console.log(isPrime(num2)); // false
