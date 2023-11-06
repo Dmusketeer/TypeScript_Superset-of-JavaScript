c56319fc33f57e15ebd20d95880cfa2ea43f23f2
Dmuskeeter <tdheeraj9a@gmail.com>
2023-11-06 08:57:17 +0530

    sqrt of a number

---
+++ Solution/034.ts
@@ -0,0 +1,9 @@
+// 34. Create a TypeScript program to find the square root of a number.
+
+const squareRoot=(num:number)=>{
+    return Math.sqrt(num)
+}
+
+console.log(squareRoot(12))
+console.log(squareRoot(16))
+console.log(squareRoot(256))
