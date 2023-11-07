2e2eddc9be3f4e0eb06d65d4b877cec0e1c08bd8
Dmuskeeter <tdheeraj9a@gmail.com>
2023-11-07 12:38:08 +0530

    remove specific element from an array

---
+++ Solution/038.ts
@@ -0,0 +1,16 @@
+// 38. Write a TypeScript function to remove a specific element from an array.
+const removeSpecificElement = (arr: number[], number: number): string | number[] => {
+    for (const element of arr) {
+        if (!arr.includes(number)) {
+            return `${number} is not present in the array`;
+        }
+        if (element == number) {
+            arr.splice(arr.indexOf(element), 1)
+        }
+    }
+    return arr
+}
+const arr1: number[] = [2, 4, 10, 3, 24]
+console.log(removeSpecificElement(arr1, 3))
+console.log(removeSpecificElement(arr1, 10))
+console.log(removeSpecificElement(arr1, 23))
